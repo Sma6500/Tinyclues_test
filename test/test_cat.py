3e33aeb9dfb032cc160216e727ab95bc25490cd7
Sma6500 <56174379+Sma6500@users.noreply.github.com>
2020-12-09 20:33:11 +0100

    Add files via upload

---
+++ test/test_cat.py
@@ -0,0 +1,36 @@
+"""
+This file contains a test for the class similarity_cat, 
+I just raise an error in case of dysfunctionnement. A best  
+method would be to test each class function and raise an appropriate error.
+"""
+
+import pandas as pd
+import numpy as np
+from abc import ABC, abstractmethod
+import os
+
+import sys
+sys.path.append(sys.path[0][:len(sys.path[0])-5])
+from similarity.similarity_cat import *
+
+path=r'path to data'
+path=r'C:/Users/luthe/Documents/AI/Tinyclues_test/Tinyclues_recommandation_algorithm/data'
+
+items_filename='movies.csv'
+ratings_filename='ratings.csv'
+users_filename='users.csv'
+
+items_path=os.path.join(path, items_filename)
+ratings_path=os.path.join(path, ratings_filename)
+users_path=os.path.join(path, users_filename)
+
+items=pd.read_csv(items_path)
+ratings=pd.read_csv(ratings_path)
+users=pd.read_csv(users_path)
+
+item_keys='movie_id'
+
+test=Recommendation_cat(items, users, ratings, categories=get_item_cat(), item_keys=item_keys)
+
+if len(test.get_recommendations(np.random.randint(0,len(items))))!=5:
+    raise 'error'
